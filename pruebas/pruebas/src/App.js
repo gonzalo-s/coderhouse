3ff01fa90edaae2ed34e4f49526ebe1c93e8cc79
Gonzalo Soler <gonzalosoler@gmail.com>
2020-08-20 10:06:32 -0300

    proyecto de pruebas, flexbox

---
+++ pruebas/pruebas/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+	return (
+		<div className="container">
+			<div className="blue-box">Blue</div>
+			<div className="green-box">Green</div>
+			<div className="red-box">Red</div>
+			<div className="yellow-box">Yellow</div>
+		</div>
+	)
+}
+
+export default App
--- tienda-app/src/components/Navbar.js
+++ tienda-app/src/components/Navbar.js
@@ -5,11 +5,14 @@ import { Link } from 'react-router-dom'
 function NavBar() {
 	return (
 		<nav>
-			<Link to="/">
-				<h3>Logo</h3>
-			</Link>
+			<div className="hamburguer">
+				<div className="line"></div>
+				<div className="line"></div>
+				<div className="line"></div>
+			</div>
+
 			<ul className="navList">
-				<Link to="/">
+				<Link className="list-link" to="/">
 					<li>Inicio</li>
 				</Link>
 				<Link to="/nosotros">
