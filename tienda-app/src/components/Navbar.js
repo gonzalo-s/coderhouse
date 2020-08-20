f1b4147c3467da3ae9de1f5f86b9ef4720743d02
Gonzalo Soler <gonzalosoler@gmail.com>
2020-08-20 12:51:39 -0300

    navbar responsive pero sin terminar el boton del menu y sin ocultar menu vertical todavia

--- tienda-app/src/components/Navbar.js
+++ tienda-app/src/components/Navbar.js
@@ -3,31 +3,52 @@ import './navBar.css'
 import { Link } from 'react-router-dom'
 // VER https://reactrouter.com/web/guides/quick-start
 function NavBar() {
+	let hamState = false
+
+	function hamStateToggle() {
+		hamState = !hamState
+		console.log(hamState)
+
+		let xBtnE = document.getElementById('xBtn')
+		let lineBtnE = document.getElementsByClassName('line')
+
+		if (xBtnE.className === 'xMenuOff') {
+			xBtnE.className = 'xMenuOn'
+			lineBtnE.className = 'lineOff'
+		} else {
+			xBtnE.className = 'xMenuOff'
+			lineBtnE.className = 'lineOn'
+		}
+	}
+
 	return (
 		<nav>
-			<div className="hamburguer">
+			<div className="hamburguer" onClick={hamStateToggle}>
 				<div className="line"></div>
 				<div className="line"></div>
 				<div className="line"></div>
+				<div id="xBtn" className="xMenuOff">
+					X
+				</div>
 			</div>
 
 			<ul className="navList">
 				<Link className="list-link" to="/">
 					<li>Inicio</li>
 				</Link>
-				<Link to="/nosotros">
+				<Link className="list-link" to="/nosotros">
 					<li>Nosotros</li>
 				</Link>
-				<Link to="/productos">
+				<Link className="list-link" to="/productos">
 					<li>Productos</li>
 				</Link>
-				<Link to="/contacto">
+				<Link className="list-link" to="/contacto">
 					<li>Contacto</li>
 				</Link>
-				<Link to="/carrito">
+				<Link className="list-link" to="/carrito">
 					<li>Carrito</li>
 				</Link>
-				<Link to="/buscar">
+				<Link className="list-link" to="/buscar">
 					<li>Buscar</li>
 				</Link>
 			</ul>
