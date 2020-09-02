513bc0600db0e2d90fecda89a63ee3c19ef24951
Gonzalo Soler <gonzalosoler@gmail.com>
2020-09-01 23:11:21 -0300

    agrega min, max con sus inputs con useState

--- pruebas/contador/src/components/Contador.js
+++ pruebas/contador/src/components/Contador.js
@@ -1,23 +1,63 @@
 import React, { useState } from 'react'
 import Button from './Button.js'
 import Display from './Display.js'
+import Input from './Input.js'
 
 function Contador() {
-	const [counter, setCounter] = useState(0)
+	const [state, setState] = useState({
+		counter: 0,
+		min: 0,
+		max: 10,
+	})
+	console.log(state)
 
 	function sumar() {
-		setCounter((counter) => counter + 1)
+		if (state.counter < state.max) {
+			setState({
+				...state,
+				counter: state.counter + 1,
+			})
+		}
 	}
 	function restar() {
-		setCounter((counter) => counter - 1)
+		if (state.counter > state.min) {
+			setState({
+				...state,
+				counter: state.counter - 1,
+			})
+		}
+	}
+
+	function handleOnChangeMin(e) {
+		console.log('change')
+		setState({
+			...state,
+			min: e.target.value,
+		})
+	}
+
+	function handleOnChangeMax(e) {
+		console.log('change')
+		setState({
+			...state,
+			max: e.target.value,
+		})
 	}
 
 	return (
 		<div>
 			<Button sign={'-'} onClick={restar} />
-			<Display contador={counter} />
+			<Display contador={state.counter} />
 			<Button sign={'+'} onClick={sumar} />
+
+			<div>
+				Min: <Input onChange={handleOnChangeMin} val={state.min} />
+			</div>
+			<div>
+				Max: <Input onChange={handleOnChangeMax} val={state.max} />
+			</div>
 		</div>
 	)
 }
+
 export default Contador
